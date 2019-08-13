a235e3c5b3a4c627561c4c7e02888bf875ffcd32
Caitlin Snyder <caitsnyder@gmail.com>
2019-08-13 14:25:28 -0400

    Ex3 without style

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,40 @@
+class ArticlesController < ActionController::Base
+  before_action :set_article, only: [:show, :update, :edit, :update, :destroy]
+
+  def article_params
+    params.require(:article).permit(:title, :details, :completed)
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    article = Article.new(article_params)
+    article.save
+    redirect_to article_path(article.id)
+  end
+
+  def update
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+end
